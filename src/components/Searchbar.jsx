7b7fdba2865a43d3e109fa9586baa5e3ef9ebaec
buburuzaaaaa <andrew4joy2015@gmail.com>
2024-01-09 15:03:36 -0700

    more

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -6,13 +6,27 @@ const Searchbar = () => {
         window.open('https://www.google.com/search?q='+ search, "_blank")
         setSearch("")
     }
+    const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  };
   return (
     <>
-    <label htmlFor="srch"></label>
-    <input type='text' className='srch'placeholder='search' id="srch" name='srch' onChange={(e)=>{setSearch(e.target.value)}}></input>
-    <button onClick={handleClick}>search</button>
+      <label htmlFor="srch"></label>
+      <input
+        type="text"
+        className="srch"
+        placeholder="search"
+        id="srch"
+        name="srch"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleClick}>search</button>
     </>
-  )
-}
+  );
+};
 
 export default Searchbar
