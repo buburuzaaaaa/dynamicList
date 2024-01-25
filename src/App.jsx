9bef3624af43ab5ce016daf646ea5ca4b91a5ac3
buburuzaaaaa <andrew4joy2015@gmail.com>
2024-01-25 15:17:21 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
 import Dynamiclist from './components/Dynamiclist';
 import Searchbar from './components/Searchbar';
 import Accordion  from './components/Accordion';
 import RandomizedQuote from './components/RandomizedQuote'
 import Question  from './components/Question';
+
 function App() {
-  // const [popupVisible, setPopupVisible] = useState(false);
-  // const [activeApp, setActiveApp] = useState(null);
+  const [spotifyVisible, setSpotifyVisible] = useState(false);
 
-  // const openPopup = (appId) => {
-  //   setPopupVisible(true);
-  //   setActiveApp(appId);
-  // };
-
-  // const closePopup = () => {
-  //   setPopupVisible(false);
-  //   setActiveApp(null);
-  // };
-
-  // const handleAppClick = (appId) => {
-  //   // Toggle the popup visibility for the clicked app
-  //   if (activeApp === appId) {
-  //     closePopup();
-  //   } else {
-  //     openPopup(appId);
-  //   }
-  // };
+  const toggleSpotify = () => {
+    setSpotifyVisible(!spotifyVisible);
+  };
 
   return (
-    
     <div className="App">
-      {/* <div id="apps-container">
-      <div className="app-box" id='app2' onClick={() => handleAppClick('Spotify')}>
-        </div>
-        <div className="app-box" id='app1' onClick={() => handleAppClick('Quote Gen')}>
-        </div>
-        </div> */}
-      <Searchbar/>
+      <Searchbar />
       <Accordion />
-      <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/4uVoHqqYPREMukeStfZNmZ?utm_source=generator" width="50%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <RandomizedQuote/>
-
-      
+      <div className="spotify-container" onClick={toggleSpotify}>
+        Click here to {spotifyVisible ? 'hide' : 'show'} Spotify
+      </div>
+      {spotifyVisible && (
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/4uVoHqqYPREMukeStfZNmZ?utm_source=generator"
+          width="50%" height="352" frameBorder="0" allowFullScreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"
+        ></iframe>
+      )}
+      <RandomizedQuote />
     </div>
-  
   );
 }
 
